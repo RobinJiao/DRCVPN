f9e2f0eb2929b325d1574f75d735a15f19b4f958
Robin Jiao <Robin.jiao@gmail.com>
2019-02-25 23:01:42 +0800

    hide the server IP and secret code

--- CreateDRCVPN.ps1
+++ CreateDRCVPN.ps1
@@ -1,4 +1,4 @@
-Add-vpnconnection -Name DRCVPN -ServerAddress 111.207.128.194 -TunnelType l2tp -AuthenticationMethod pap -SplitTunneling -RememberCredential -l2tpPsk greatwall -Force
+Add-vpnconnection -Name DRCVPN -ServerAddress x.x.x.x -TunnelType l2tp -AuthenticationMethod pap -SplitTunneling -RememberCredential -l2tpPsk ****** -Force
 Add-VpnConnectionRoute -ConnectionName "DRCVPN" -DestinationPrefix 74.143.206.0/24 
 Add-VpnConnectionRoute -ConnectionName "DRCVPN" -DestinationPrefix 68.71.0.0/16
 Add-VpnConnectionRoute -ConnectionName "DRCVPN" -DestinationPrefix 132.0.0.0/8
